a81df1536912b90be4dd349bd075e8135aaf38e7
bap37 <bap37@duke.edu>
2021-12-20 15:05:17 -0600

    New DUST2DUST main file, significant refactoring

--- callSALT2mu.py
+++ callSALT2mu.py
@@ -20,10 +20,7 @@ def setup_custom_logger(name, screen=False):
     if screen:
         logger.addHandler(screen_handler)
     return logger
-
-
-massarr = np.arange(5.,15.,1)
-zarr = np.linspace(-2.4,2.6,3)
+    #END setup_custom_logger
 
 class SALT2mu: #I understand classes better now
     def __init__(self,command,mapsout,SALT2muout,log,realdata=False,debug=False): #setting all sorts of class values
@@ -65,24 +62,18 @@ class SALT2mu: #I understand classes better now
                                universal_newlines=True)
             self.logger.info('Command being run: ' + (self.command%(mapsout,SALT2muout,log))) 
             self.stdout_iterator = iter(self.process.stdout.readline, "")
-            #self.getResult()  
-
-            #self.call()
-            #self.next()
-            #self.getResult()
-
-    #def call(self):
-    #    self.getResult()
-    #    return self.data
+        #END __init__
 
     def next(self): #ticks up iteration by one 
         print('writing next iter to stdin')
         self.process.stdin.write('%d\n'%self.iter)  
+        #END next
         
     def quit(self): #sets iteration input to -1, which causes a quit somewhere
         self.process.stdin.write('-1\n')
         for stdout_line in iter(self.process.stdout.readline, ""): 
             print(stdout_line)
+        #END quit
 
     def getResult(self):
         start = False #ok fine
@@ -98,65 +89,75 @@ class SALT2mu: #I understand classes better now
                 if str(self.ready) in stdout_line:
                     #self.data = self.getData()
                     return
-    
             else:
                 #if self.ready2%(self.iter-1) in stdout_line:
                 if str(self.ready) in stdout_line:
                     print('getting data')
                     self.data = self.getData()                
                     return 
+        #END getResult
 
     def getData(self): #reads in the current SALT2mu output fitres and scrapes data.
-        #import ipdb; ipdb.set_trace()
         self.SALT2muoutputs.seek(0) #sets pointer to top of file
         text = self.SALT2muoutputs.read() #reads in the text
-##        print((text), 'heller')
         self.alpha = float(text.split('alpha0')[1].split()[1]) 
         self.alphaerr = float(text.split('alpha0')[1].split()[3])
         self.beta = float(text.split('beta0')[1].split()[1])
         self.betaerr = float(text.split('beta0')[1].split()[3])
         self.maxprob = float(text.split('MAXPROB_RATIO')[1].split()[1])
-        #print(self.maxprob)
         self.headerinfo = self.NAndR(StringIO(text))
         self.sigint = float(text.split('sigint')[1].split()[1])
-        #self.cbindf = pd.read_csv(StringIO(text),comment='#',delim_whitespace=True) #this appears to be the fitres file 
         self.bindf = pd.read_csv(StringIO(text), header=None, skiprows=self.headerinfo[1],names=self.headerinfo[0], delim_whitespace=True, comment='#')
         return True
+        #END getData
 
     def get_1d_asym_gauss(self,mean,lhs,rhs,arr): #creates a 1d, asymmetric Gaussian
         probs = np.exp(-.5*((arr-mean)/lhs)**2) 
         probs[arr>mean] = np.exp(-.5*((arr[arr>mean]-mean)/rhs)**2)
         probs = probs/np.max(probs)
         return arr,probs #x and y
+        #END get_1d_asym_gauss
 
     def get_1d_exponential(self, tau, arr):
         probs = (tau**-1)*np.exp(-arr/tau)
         probs = probs/np.max(probs)
         return arr, probs
+        #END get_1d_exponential
 
     def get_1d_lognormal(self, mu, std, arr):
         probs = np.exp(mu + std*arr)
         probs = probs/np.max(probs)
         return arr, probs
+        #END get_1d_lognormal
 
-    def writeheader(self,names): #writes the header 
+    def writeheader(self,names): #writes the header - DEPRECATED
         self.crosstalkfile.write('VARNAMES:')
         for name in names:
             self.crosstalkfile.write(' '+name)
         self.crosstalkfile.write(' PROB\n')
         return 
+        #END writeheader
+
+    def writegenericheader(self, inp, varnames):
+        self.crosstalkfile.write(f'VARNAMES: {inp}')
+        for name in varnames: self.crosstalkfile.write(' '+name); 
+        self.crosstalkfile.write(' PROB \n')
+        return
+        #END writegenericheader
 
     def write3Dprobs(self,arr,z,mass,probs): #Writes a 3D probability    
         bigstr = ''    
         for a,p in zip(arr,probs): 
             bigstr+='PDF: %.3f %.2f %.2f %.3f\n'%(a,z,mass,p)        
         self.crosstalkfile.write(bigstr)        
+        #END write3Dprobs
 
     def write2Dprobs(self,arr,mass,probs): #Writes a 2D probability 
         bigstr = ''
         for a,p in zip(arr,probs):
             bigstr+='PDF: %.3f %.2f %.3f\n'%(a,mass,p)
         self.crosstalkfile.write(bigstr)
+        #END write2Dprobs
 
     def write1Dprobs(self,arr,probs): #Writes a 1D probability 
         bigstr = ''
@@ -164,15 +165,18 @@ class SALT2mu: #I understand classes better now
             bigstr+='PDF: %.3f %.3f\n'%(a,p)
         bigstr+='\n'
         self.crosstalkfile.write(bigstr)
+        #END write1Dprobs
 
     def write_iterbegin(self):
         self.crosstalkfile.truncate(0)
         self.crosstalkfile.seek(0)
         self.crosstalkfile.write('ITERATION_BEGIN: %d\n'%self.iter)
+        #END write_iterbegin
 
     def write_iterend(self):
         self.crosstalkfile.write('ITERATION_END: %d\n'%self.iter)
         self.crosstalkfile.flush()
+        #END write_iterend
 
     def write_SALT2(self, name, PARAMS):
         for tm in range(3):    
@@ -187,9 +191,9 @@ class SALT2mu: #I understand classes better now
             self.crosstalkfile.write(f"GENRANGE_SIM_{name}: .4 3 \n")
         for tm in range(3):
             self.crosstalkfile.write("\n")
+        #END write_SALT2
 
-
-    def write_1D_PDF(self,varname, PARAMS, arr):
+    def write_1D_PDF(self,varname, PARAMS, arr): #DEPRECATED
         self.writeheader([varname])
         try:
             mean,lhs,rhs = PARAMS
@@ -199,8 +203,9 @@ class SALT2mu: #I understand classes better now
             rhs = lhs
         arr,probs = self.get_1d_asym_gauss(mean,lhs,rhs,arr)
         self.write1Dprobs(arr,probs)
+        #END write_1D_PDF
 
-    def writeRVLOGMASS_PDF(self,mean,lhs,rhs):
+    def writeRVLOGMASS_PDF(self,mean,lhs,rhs): #DEPRECATED
         self.writeheader(['RV','LOGMASS'])
         for mass in massarr:
             if mass < 10:
@@ -210,8 +215,9 @@ class SALT2mu: #I understand classes better now
                 arr,probs = self.get_1d_asym_gauss(mean,lhs,rhs)
             probs[arr<.5] = 0
             self.write2Dprobs(arr,mass,probs)
+        #END writeRVLOGMASS_PDF
 
-    def write_2D_Mass_PDF(self, varname, PARAMS, arr):
+    def write_2D_Mass_PDF(self, varname, PARAMS, arr): #DEPRECATED
         self.writeheader([varname, 'HOST_LOGMASS'])
         Lmean, Llhs, Lrhs = PARAMS[0:3]
         Hmean, Hlhs, Hrhs = PARAMS[3:]        
@@ -225,8 +231,9 @@ class SALT2mu: #I understand classes better now
                 probs[arr < .4] = 0
                 self.write2Dprobs(arr,mass,probs)
         self.crosstalkfile.write('\n')
+        #END write_2D_Mass_PDF
 
-    def write_2D_Mass_PDF_SYMMETRIC(self, varname, PARAMS, arr):       
+    def write_2D_Mass_PDF_SYMMETRIC(self, varname, PARAMS, arr):  #DEPRECATED
         self.writeheader([varname, 'HOST_LOGMASS'])                 
         Lmean, Llhs = PARAMS[0:2]
         Lrhs = Llhs
@@ -242,8 +249,9 @@ class SALT2mu: #I understand classes better now
                 probs[arr < .4] = 0        
                 self.write2Dprobs(arr,mass,probs)      
         self.crosstalkfile.write('\n') 
+        #END write_2D_Mass_PDF_SYMMETRIC
 
-    def write_2D_MassEBV_PDF(self, varname, PARAMS, arr):  
+    def write_2D_MassEBV_PDF(self, varname, PARAMS, arr):  #DEPRECATED
         self.writeheader([varname, 'HOST_LOGMASS'])       
         LTau = PARAMS[0]
         HTau = PARAMS[1]  
@@ -255,8 +263,9 @@ class SALT2mu: #I understand classes better now
                 arr,probs = self.get_1d_exponential(HTau,arr)         
                 self.write2Dprobs(arr,mass,probs)
         self.crosstalkfile.write('\n')
+        #END write_2D_MassEBV_PDF
 
-    def write_2D_LOGNORMAL_PDF(self, varname, PARAMS, arr):
+    def write_2D_LOGNORMAL_PDF(self, varname, PARAMS, arr): #DEPRECATED
         self.writeheader([varname, 'HOST_LOGMASS']) 
         Lmu, Lstd = PARAMS[0,1]
         Hmu, Hstd = PARAMS[2,3]    
@@ -268,8 +277,9 @@ class SALT2mu: #I understand classes better now
                 arr,probs = self.get_1d_lognormal(Hmu, Hstd, arr)
                 self.write2Dprobs(arr,mass,probs)
         self.crosstalkfile.write('\n')  
+        #END write_2D_LOGNORMAL_PDF
 
-    def write_3D_MassEBV_PDF(self, varname, PARAMS, arr):   #for when EBV needs a z split
+    def write_3D_MassEBV_PDF(self, varname, PARAMS, arr):   #for when EBV needs a z split - DEPRECATED
         self.writeheader([varname, 'SIM_ZCMB', 'HOST_LOGMASS']) #needs work   
         LZ_LTau = PARAMS[0]    
         LZ_HTau = PARAMS[1]   
@@ -293,8 +303,9 @@ class SALT2mu: #I understand classes better now
                         arr,probs = self.get_1d_exponential(HZ_HTau,arr) 
                         self.write3Dprobs(arr,z,mass,probs)  
         self.crosstalkfile.write('\n') 
+        #END write_3D_MassEBV_PDF
 
-    def write_3D_LOGNORMAL_PDF(self, varname, PARAMS, arr):   #for when EBV needs a z split 
+    def write_3D_LOGNORMAL_PDF(self, varname, PARAMS, arr):   #for when EBV needs a z split - DEPRECATED
         self.writeheader([varname, 'SIM_ZCMB', 'HOST_LOGMASS']) #needs work  
         LZ_Lmu, LZ_Lstd, LZ_Hmu, LZ_Hstd, HZ_Lmu, HZ_Lstd, HZ_Hmu, HZ_Hstd = PARAMS 
         for z in zarr:    
@@ -315,8 +326,9 @@ class SALT2mu: #I understand classes better now
                         arr,probs = self.get_1d_lognormal(HZ_Hmu,HZ_Hstd,arr) 
                         self.write3Dprobs(arr,z,mass,probs)   
         self.crosstalkfile.write('\n') 
+        #END write_3D_LOGNORMAL_PDF
 
-    def write_2D_PDF(self, varname, LOWPARAMS, HIGHPARAMS, arr):     
+    def write_2D_PDF(self, varname, LOWPARAMS, HIGHPARAMS, arr):#DEPRECATED
         self.writeheader([varname[0], varname[1]])                    
         Lmean, Llhs, Lrhs = LOWPARAMS
         Hmean, Hlhs, Hrhs = HIGHPARAMS 
@@ -327,6 +339,7 @@ class SALT2mu: #I understand classes better now
                 arr,probs = self.get_1d_asym_gauss(Hmean,Hlhs,Hrhs)                             
             probs[arr<.5] = 0                             
             self.write2Dprobs(arr,mass,probs) 
+        #END write_2D_PDF
 
     def NAndR(self, fp):
         for i, line in enumerate(fp):
@@ -338,3 +351,72 @@ class SALT2mu: #I understand classes better now
                 Startrow = i
                 break  
         return Names, Startrow 
+        #END NAndR
+        
+    def write_generic_PDF(self, inp, SPLIT, PARAMS, SHAPE, SHAPEDICT,SIMDICT, arr):
+        if ('alpha' in inp) or ('beta' in inp): #quick check to see if it's one of the weird parameters.
+            self.write_SALT2(inp, PARAMS)
+            return 'Done' #Ends the function early because beta/alpha don't work as below.
+        splits = [] #Creates empty array for consistency
+        if inp in SPLIT.keys(): splits = list(SPLIT[inp].keys()) #if splits exist for this inp, lists
+        splits = [SIMDICT[i] for i in splits] #convert to SNANA readable format
+        splitloc = [SPLIT[inp][i] for i in splits]
+        self.writegenericheader(SIMDICT[inp], splits) #Done writing the header.
+        if len(splits) == 0:
+            arrs,probs= self.shape_assigner(PARAMS, SHAPE, arr[0])
+            if inp == 'RV': probs[arrs < .4] = 0
+            self.write1Dprobs(arrs, probs)
+        elif len(splits) == 1:
+            SPLITPARAMS = self.shape_interpret(PARAMS, inp, SHAPE, SHAPEDICT, SPLIT)
+            for sp1 in arr[1]:
+                if sp1 < splitloc[0]:
+                    arrs,probs= self.shape_assigner(SPLITPARAMS[0], SHAPE, arr[0])
+                    if inp == 'RV': probs[arrs < .4] = 0
+                    self.write2Dprobs(arrs,sp1, probs)
+                else:
+                    arrs,probs= self.shape_assigner(SPLITPARAMS[1], SHAPE, arr[0])
+                    if inp == 'RV': probs[arrs < .4] = 0
+                    self.write2Dprobs(arrs, sp1, probs)
+        elif len(splits) == 2:
+            SPLITPARAMS = self.shape_interpret(PARAMS, inp, SHAPE, SHAPEDICT, SPLIT)
+            for sp1 in arr[1]: #starts first split "low" version 
+                if np.around(sp1,3) < splitloc[0]: #if first split condition is met
+                    for sp2 in arr[2]: #Second split 
+                        if np.around(sp2,1) < splitloc[1]: #Start second split "low" version
+                            arrs,probs= self.shape_assigner(SPLITPARAMS[0], SHAPE, arr[0])
+                            if inp == 'RV': probs[arrs < .4] = 0
+                            self.write3Dprobs(arrs,sp1,sp2,probs)
+                        else:
+                            arrs,probs= self.shape_assigner(SPLITPARAMS[1], SHAPE, arr[0])
+                            if inp == 'RV': probs[arrs < .4] = 0
+                            self.write3Dprobs(arrs,sp1,sp2,probs)
+                else: #starts first split "high" version
+                    for sp2 in arr[2]: #Start second split
+                        if np.around(sp2,1) < splitloc[1]: #Starts second split "low" version
+                            arrs,probs= self.shape_assigner(SPLITPARAMS[2], SHAPE, arr[0])
+                            if inp == 'RV': probs[arrs < .4] = 0
+                            self.write3Dprobs(arrs,sp1,sp2,probs)
+                        else:
+                            arrs,probs= self.shape_assigner(SPLITPARAMS[3], SHAPE, arr[0])
+                            if inp == 'RV': probs[arrs < .4] = 0
+                            self.write3Dprobs(arrs,sp1,sp2,probs)
+        self.crosstalkfile.write('\n')
+        #END write_generic_PDF
+
+    def shape_assigner(self, PARAMS, SHAPE, arr):
+        if SHAPE == 'Gaussian':
+            return self.get_1d_asym_gauss(PARAMS[0], PARAMS[1], PARAMS[1],arr)
+        elif SHAPE == 'Exponential':
+            return self.get_1d_exponential(PARAMS[0], arr)
+        elif SHAPE == 'LogNormal':
+            return self.get_1d_lognormal(PARAMS[0], PARAMS[1], arr)
+        elif SHAPE == 'Skewed Gaussian':
+            return self.get_1d_asym_gauss(PARAMS[0], PARAMS[1], PARAMS[2],arr)
+        #END shape_assigner
+
+    def shape_interpret(self, PARAMS, inp, SHAPE, SHAPEDICT, SPLIT):       
+        temp = []                                                             
+        for i in range(len(SPLIT[inp].keys())*2):  
+            temp.append(PARAMS[0+i*len(SHAPEDICT[SHAPE]):(i+1)*len(SHAPEDICT[SHAPE])]) 
+        return temp 
+        #END shape_interpret
